6ec89d09ca4ee3e9c8246366001ca0874601df52
wx <wxnderer@cyberfear.com>
2024-04-29 15:20:42 +0200

    Feat: Play sound on replay buffer start/stop event

--- obs-notify.py
+++ obs-notify.py
@@ -8,6 +8,7 @@ def script_properties():
     obs.obs_properties_add_bool(props, "show_notification", "Play windows notification instead of sound")
     obs.obs_properties_add_bool(props, "custom_audio", "Use custom audio file")
     obs.obs_properties_add_path(props, "wav_path", "WAV Audio File Path", obs.OBS_PATH_FILE, "WAV Files (*.wav)", None)
+    obs.obs_properties_add_bool(props,"play_start_stop_audio", "Play Audio for Replay Buffer Start/Stop Event")
     
     return props
 
@@ -19,11 +20,12 @@ def script_load(settings):
 
 def event_callback(event):
     obs.script_log(obs.LOG_INFO, str(event))
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    wav_path = obs.obs_data_get_string(global_settings, "wav_path")
+    show_notification_setting = obs.obs_data_get_bool(global_settings, "show_notification")
+    custom_audio = obs.obs_data_get_bool(global_settings, "custom_audio")
+    playStartStopEvent = obs.obs_data_get_bool(global_settings, "play_start_stop_audio")
     if event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED:
-        script_path = os.path.dirname(os.path.realpath(__file__))
-        wav_path = obs.obs_data_get_string(global_settings, "wav_path")
-        show_notification_setting = obs.obs_data_get_bool(global_settings, "show_notification")
-        custom_audio = obs.obs_data_get_bool(global_settings, "custom_audio")
         if show_notification_setting:
             show_notification("OBS Replay Buffer", "Your clip has been successfully saved!")
         else:
@@ -33,6 +35,13 @@ def event_callback(event):
                 play_sound(os.path.join(script_path, "obs-notify.wav"))
 
         obs.script_log(obs.LOG_INFO, "Replay buffer saved")
+
+    if playStartStopEvent and event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED:
+        obs.script_log(obs.LOG_INFO, "Recording stopped")
+        play_sound(os.path.join(script_path, "obs-replay-buffer-stopped.wav"))
+    if playStartStopEvent and event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED:
+        obs.script_log(obs.LOG_INFO, "Recording started")
+        play_sound(os.path.join(script_path, "obs-replay-buffer-started.wav"))
 
 def script_unload():
     obs.script_log(obs.LOG_INFO, "Python script unloaded")
